73e2fd017513a41c56124535f480040c6761c2c1
Riripii <85088379+Riripii@users.noreply.github.com>
2021-08-04 18:09:39 +0700

    Update main.js

--- source/main.js
+++ source/main.js
@@ -7,4 +7,13 @@ source.get('/', (req, res) => {
     res.sendFile(__path + '/template/home.html')
 })
 
+source.get('/config', (req, res) => {
+    config = {
+        status: 200,
+        info: 'GabutzApi',
+        bot: 'Yazawabotz'
+    }
+    res.json(config)
+})
+
 module.exports = source
